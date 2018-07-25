fdd3bdd878830cfd1f8dabe6ffe3957e41469c8d
562794175 <562794175@qq.com>
2018-07-25 16:31:43 +0800

    xg

--- tensorflow_demo/com/test4.py
+++ tensorflow_demo/com/test4.py
@@ -1,8 +1,21 @@
-import gym
-import gym.spaces
+import zmq
+import random
+import sys
+import time
 
-env = gym.make('CartPole-v0')
-env.reset()
+port = "5556"
+context = zmq.Context()
 
-for _ in range(1000):
-    env.render()
+socket_server = context.socket(zmq.PAIR)
+socket_server.bind("tcp://*:%s" % port)
+
+socket = context.socket(zmq.PAIR)
+socket.connect("tcp://localhost:%s" % port)
+
+for i in range(10):
+        socket.send_string("client message to server "+ str(i))
+
+while True:
+        msg = socket_server.recv()
+        print(msg)
+        time.sleep(20)
