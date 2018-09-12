6df2a6e882f09e8e10d4292347c228270367cbef
562794175 <562794175@qq.com>
2018-09-12 16:50:12 +0800

    xg

--- xau.sql
+++ xau.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50714
 File Encoding         : 65001
 
-Date: 2018-09-11 17:08:04
+Date: 2018-09-12 16:49:53
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,8 +23,19 @@ CREATE TABLE `xau` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `begin` datetime NOT NULL,
   `end` datetime NOT NULL,
-  `data` varchar(1024) NOT NULL COMMENT 'JSON格式',
   `peroid` int(11) DEFAULT NULL,
-  `class_id` int(11) DEFAULT NULL COMMENT '类别',
+  `data_price` varchar(1024) NOT NULL COMMENT 'JSON格式',
+  `data_price_class` tinyint(5) DEFAULT NULL,
+  `data_volume` varchar(1024) NOT NULL,
+  `data_volume_class` tinyint(5) DEFAULT NULL,
+  `data_rsi` varchar(1024) DEFAULT NULL,
+  `data_rsi_class` tinyint(5) DEFAULT NULL,
+  `action` tinyint(5) DEFAULT NULL,
+  `action_year` varchar(10) DEFAULT NULL,
+  `action_month` varchar(10) DEFAULT NULL,
+  `action_day` varchar(10) DEFAULT NULL,
+  `action_hour` varchar(10) DEFAULT NULL,
+  `action_minute` varchar(10) DEFAULT NULL,
+  `action_second` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
