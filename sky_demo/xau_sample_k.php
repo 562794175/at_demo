ceed3c50caf018461874c317f7530f57fc78e334
562794175 <562794175@qq.com>
2018-11-08 11:32:21 +0800

    xg

--- sky_demo/xau_sample_k.php
+++ sky_demo/xau_sample_k.php
@@ -12,33 +12,55 @@ $resultall = $db->query($sqlall);
 $arr1 = $resultall->fetch_row();
 $c = $arr1[0];
 $pageindex=empty($_GET["page"])?0:$_GET["page"];
-$page = new page($c,2);
+$page = new page($c,1);
 $sql = "select * from xau_15 ".$page->limit;
 $result = $db->query($sql);
 $arr = $result->fetch_all();
 $kdata=[];
 $highData=$lowData=$openData=$closeData=[];
+$lowerDataBolling=$mainDataBolling=$upperDataBolling=[];
 $id=0;
 foreach($arr as $v){
     $id=$v[0];
     $timeframe=$v[1];
     $data_price=$v[4];
     $data_price = json_decode($data_price,TRUE);
-    $ar=explode(",", $data_price["high"]);
-    array_pop($ar);
-    $highData = array_merge($highData,$ar);
-    
-    $ar=explode(",", $data_price["low"]);
-    array_pop($ar);
-    $lowData = array_merge($lowData,$ar);
-    
-    $ar=explode(",", $data_price["open"]);
-    array_pop($ar);
-    $openData = array_merge($openData,$ar);
-    
-    $ar=explode(",", $data_price["close"]);
-    array_pop($ar);
-    $closeData = array_merge($closeData,$ar);
+    $highData=explode(",", $data_price["high"]);
+    $lowData=explode(",", $data_price["low"]);
+    $openData=explode(",", $data_price["open"]);
+    $closeData=explode(",", $data_price["close"]);
+    $data_bolling = $v[8];
+    $data_bands = json_decode($data_bolling,TRUE);
+    $lowerDataBolling = explode(",", $data_bands["lower"]);
+    $mainDataBolling = explode(",", $data_bands["main"]);
+    $upperDataBolling = explode(",", $data_bands["upper"]);
+}
+//查找下一条记录
+$sql = "select * from xau_15 where id=".($id+1);
+$result = $db->query($sql);
+$arr_next = $result->fetch_all();
+foreach($arr_next as $v){
+    $data_price=$v[4];
+    $data_price = json_decode($data_price,TRUE);
+    $hd=explode(",", $data_price["high"]);
+    $ld=explode(",", $data_price["low"]);
+    $od=explode(",", $data_price["open"]);
+    $cd=explode(",", $data_price["close"]);
+//    $data_bolling = $v[8];
+//    $data_bands = json_decode($data_bolling,TRUE);
+//    $ldb = explode(",", $data_bands["lower"]);
+//    $mdb = explode(",", $data_bands["main"]);
+//    $udb = explode(",", $data_bands["upper"]);
+    for($i=1;$i<4;$i++) {
+        if($i>=count($hd)) break;
+        $highData[]=$hd[$i];
+        $lowData[]=$ld[$i];
+        $openData[]=$od[$i];
+        $closeData[]=$cd[$i];
+//        $lowerDataBolling[]=$ldb[$i];
+//        $mainDataBolling[]=$mdb[$i];
+//        $upperDataBolling[]=$udb[$i];
+    }
 }
 
 for($i=2;$i<count($highData);$i++) {
@@ -56,6 +78,10 @@ $head = "<a href='xau_sample.php'>sample</a> - <a href='xau_complex.php'>complex
 echo "<div align='center'>".$head."<br>".$page->fpage()."</div>";//显示分页信息
 
 ?>
+<style>
+    td input { width:50px;}
+    #long{ width:100px;}
+</style>
 <form id="sampleForm" action="xau_sample_k.php?id=<?php echo $id;?>&dopost=ajaxsave&page=<?php echo $pageindex; ?>" method="post" >
 <table border=1 align='center'>
     <tr>
@@ -81,12 +107,15 @@ foreach($kdata as $key=> $value) {
     $data['id']=$id;
     $data['tf']=$timeframe;
     $data['sn']="-1";
-    $imageall=getCandlePng($data,600,600);
+    $bolling['l']=$lowerDataBolling;
+    $bolling['m']=$mainDataBolling;
+    $bolling['u']=$upperDataBolling;
+    $imageall=getCandlePng($data,600,600,$bolling);
     echo "<div align='center' ><img src='".$imageall."'></div>";
 ?>
 </form>
 <?php
-function getCandlePng($data,$width=50,$height=50)
+function getCandlePng($data,$width=50,$height=50,$bolling=null)
 {
     $c = new XYChart($width, $height+10);
     $plotAreaObj =$c->setPlotArea(0, 0, $width, $height);
@@ -95,6 +124,11 @@ function getCandlePng($data,$width=50,$height=50)
     $c->yAxis()->setColors(Transparent, Transparent);
     $layer = $c->addCandleStickLayer($data['h'],$data['l'], $data['o'], $data['c'], 0xFFFFFF, 0x000000);
     $layer->setLineWidth(2);
+    if(!empty($bolling)) {
+        $c->addLineLayer($bolling['l']);
+        $c->addLineLayer($bolling['m']);
+        $c->addLineLayer($bolling['u']);
+    }
     $filename=$data['id']."_".$data['tf']."_".$data['sn'].".png";
     $realpath=realpath('.')."".PATHSEP."png".PATHSEP."k".PATHSEP."".$filename;
     $c->makeChart($realpath);
